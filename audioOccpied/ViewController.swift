0d47f5121e8cc2fd625d50272ae6a3c0c4873563
xcoderliu <xcoderliu@gmail.com>
2026-01-08 15:43:44 +0800

    Initial Commit

---
+++ audioOccpied/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  audioOccpied
+//
+//  Created by 刘智民 on 2026/1/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
